9437f6aef93669fc8a890ea9ee0439a2eaaff848
Akhila Polasu <akhila.polasu@calsoftinc.com>
2024-12-14 21:41:16 +0530

    This is Automation Started

---
+++ Sampleclass.py
@@ -0,0 +1,11 @@
+class MyNum:
+    def __init__(self,a):
+        print("num is")
+        self.abc=a
+    def display(self):
+        print(self.abc)
+a1=MyNum(8)
+a1.display()
+# del a1.abc
+# print(a1)
+# a1.display()
